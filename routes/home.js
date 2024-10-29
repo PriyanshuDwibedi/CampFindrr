f921edc3c0d13886c652b928ff49c66f925dfdea
Priyanshu Dwibedi <117301531+PriyanshuDwibedi@users.noreply.github.com>
2024-10-30 01:13:37 +0530

    Add files via upload

---
+++ routes/home.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const campgrounds = require('../controllers/campgrounds');
+const catchAsync = require('../utils/catchAsync');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({storage});
+
+router.route('/home')
+    .get(isLoggedIn, catchAsync(campgrounds.home));
+    
